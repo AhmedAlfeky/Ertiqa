0bb7f8b6934c935fe8c07fd20611914b42a9325c
AhmedAlfeky <ahmedalfeky@yahoo.com>
2025-11-01 10:34:02 +0200

    تجربة 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,4 +4,7 @@ import { redirect } from 'next/navigation';
 export default function Home() {
   // إعادة توجيه تلقائي إلى اللغة الافتراضية
   redirect('/ar');
+  // إضافة مؤقتة يتم حذفها بعد الاختبار
+  return <main><h1>Ping — الصفحة تعمل</h1></main>;
+
 }

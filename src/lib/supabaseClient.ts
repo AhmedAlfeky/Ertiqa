29b4cd082bb490fee0fbc5a3d841a01bcc3ca2a8
AhmedAlfeky <ahmedalfeky@yahoo.com>
2025-11-01 09:58:20 +0200

    إضاف ملف vercel.json

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,22 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+//تم إيقاف هذا الكود لاختبار الكود الآخر
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+
+
+// للـ client: استخدام متغيرات NEXT_PUBLIC_
+export const supabaseClient = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
+
+// للخادم (إذا تحتاج صلاحيات أعلى) استخدم SERVICE_ROLE_KEY دون NEXT_PUBLIC_
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string
+);
